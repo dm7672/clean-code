fb34c3973950993306e09b6cb22f5e6892fac4e7
dm7672 <dmitrsem10@gmail.com>
2025-11-07 18:45:22 +0500

    добавил класс Tokenizer

---
+++ Markdown/Tokenizer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markdown
+{
+    internal class Tokenizer
+    {
+        public Tokenizer()
+        {
+            throw new NotImplementedException();
+        }
+        public List<Token> Tokenize(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
